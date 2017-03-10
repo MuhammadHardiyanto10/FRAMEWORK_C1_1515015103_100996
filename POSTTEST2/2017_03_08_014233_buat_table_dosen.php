d1c067ffccb2231dbe428c04d2a77f97e3c734b4
MuhammadHardiyanto10 <muhammadhardiyanto10@gmail.com>
2017-03-10 13:17:16 +0000

    Add files via upload

---
+++ POSTTEST2/2017_03_08_014233_buat_table_dosen.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BuatTableDosen extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dosen', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Nama',50);
+            $table->string('Nip',18);
+            $table->text('Alamat');
+            $table->integer('pengguna_id',false,true)->references('id')->on('pengguna')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('dosen');
+    }
+}
